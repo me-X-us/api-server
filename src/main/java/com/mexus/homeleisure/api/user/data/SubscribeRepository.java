7d060b97ca01a9b66f82ed96402ab0062ae6e249
CodingNome <si8363@naver.com>
2020-11-24 17:04:34 +0900

    fix: transactional error

--- src/main/java/com/mexus/homeleisure/api/user/data/SubscribeRepository.java
+++ src/main/java/com/mexus/homeleisure/api/user/data/SubscribeRepository.java
@@ -3,10 +3,12 @@ package com.mexus.homeleisure.api.user.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+    @Transactional
     void deleteByTrainer_UserIdAndUser_UserId(String trainerId, String userId);
     boolean existsByTrainer_UserIdAndUser_UserId(String trainerId, String userId);
     List<Subscribe> findAllByUser_UserId(String userId);
